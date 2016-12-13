15956fd5408aafd5ec0f380d63a6c7e87f1de7bd
EETEST <EETEST@10.210.201.254>
2016-12-13 17:35:13 +0000

    final for today

--- copy-project/src/main/java/com/listeners/WebDriverListener.java
+++ copy-project/src/main/java/com/listeners/WebDriverListener.java
@@ -1,28 +1,20 @@
 package com.listeners;
 
-import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.WebDriver;
-import org.testng.IInvokedMethod;
-import org.testng.IInvokedMethodListener;
-import org.testng.ITestResult;
+import org.testng.IExecutionListener;
 
-import com.driver.DriverManager;
-import com.driver.LocalDriverFactory;
 import com.utils.SetDriver;
 
-public class WebDriverListener implements IInvokedMethodListener {
+public class WebDriverListener implements IExecutionListener {
 
-	public void afterInvocation(IInvokedMethod arg0, ITestResult arg1) {
+	
+	public void onExecutionFinish() {
 		// TODO Auto-generated method stub
 		
 	}
 
-	public void beforeInvocation(IInvokedMethod method, ITestResult testResult) {
-		SetDriver.setUpChromeDriver();
-		DriverManager.setWebDriver(LocalDriverFactory.createInstance(method.getTestMethod().getXmlTest().getParameters().get("browserName")));
-		WebDriver driver = DriverManager.getDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	public void onExecutionStart() {
+	   SetDriver.setUpChromeDriver();
 		
 	}
 
